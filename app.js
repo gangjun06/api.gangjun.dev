03a17db9ceab3a8db80f1571d778699da9ab40e6
이강준 <admin@gangjun.dev>
2020-05-28 15:28:32 +0900

    add basic code

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+
+
+// Start the Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server Start, ${port}port`);
+});
