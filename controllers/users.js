b0428f6d27ae413d3db25fab8bfb556e60f9247c
이강준 <admin@gangjun.dev>
2020-05-28 18:59:03 +0900

    validate data

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,5 @@
 module.exports = {
   signUp: async (req, res, next) => {
-    console.log("signUp called");
-    res.end();
   },
   signIn: async (req, res, next) => {
     console.log("SignIn called");
