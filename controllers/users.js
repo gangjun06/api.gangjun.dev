f25261d969532c54a3d957356b5aa78a624a2691
이강준 <admin@gangjun.dev>
2020-05-29 11:41:11 +0900

    add jwt

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,38 @@
 const DB = require("./../helpers/db");
+const JWT = require("jsonwebtoken");
+const { JWT_SECRET } = require("./../config");
+
+const signToken = (user) => {
+  return JWT.sign(
+    {
+      iss: "gangjun",
+      sub: user,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1),
+    },
+    JWT_SECRET
+  );
+};
 
 module.exports = {
   signUp: async (req, res, next) => {
     const { email, password } = req.value.body;
     const loEmail = email.toLowerCase();
+    let user;
 
     try {
-      const user = await await DB("user")
-        .insert({ email: loEmail, password })
-      res.json({ user: user[0] });
+      user = await await DB("user").insert({ email: loEmail, password });
+      user = user[0];
     } catch (error) {
       if (error.code === "ER_DUP_ENTRY") {
-        return res.json({ error: "Email is already in use" });
+        return res.status(403).json({ error: "Email is already in use" });
       }
       return res.json({ error });
     }
+
+    // res.json({ user: user[0] });
+    const token = signToken(user);
+    return res.status(200).json({ user, token });
   },
   signIn: async (req, res, next) => {
     console.log("SignIn called");
