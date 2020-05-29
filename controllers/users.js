a96ea64f24948987d01ada019879660c3b978da8
이강준 <admin@gangjun.dev>
2020-05-29 11:14:42 +0900

    update db

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,20 @@
+const DB = require("./../helpers/db");
+
 module.exports = {
   signUp: async (req, res, next) => {
+    const { email, password } = req.value.body;
+    const loEmail = email.toLowerCase();
+
+    try {
+      const user = await await DB("user")
+        .insert({ email: loEmail, password })
+      res.json({ user: user[0] });
+    } catch (error) {
+      if (error.code === "ER_DUP_ENTRY") {
+        return res.json({ error: "Email is already in use" });
+      }
+      return res.json({ error });
+    }
   },
   signIn: async (req, res, next) => {
     console.log("SignIn called");
