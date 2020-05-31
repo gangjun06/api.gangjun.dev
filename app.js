8046503498cfa62a99b34cb95a7da006cf971715
이강준 <admin@gangjun.dev>
2020-05-31 16:32:11 +0900

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
 // Middlewares
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
