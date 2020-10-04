d263d23b027ba149c04810d1151f2e1066fadaa8
Gangjun Lee <me@gangjun.dev>
2020-10-04 19:37:30 +0900

    write basic struct

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
 
-func main(){
-    fmt.Println("Hello, World")
+	"github.com/gangjun06/api.gangjun.dev/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	etcInit()
+	applyConfig()
+	startServer()
+}
+
+func etcInit() {
+	rand.Seed(time.Now().Unix())
+}
+
+func applyConfig() {
+}
+
+func startServer() {
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	r.Use(middlewares.Cors())
+	r.Run(":9096")
 }
